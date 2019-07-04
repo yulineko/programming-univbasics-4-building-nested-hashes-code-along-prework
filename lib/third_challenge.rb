60cb8a6531c55b513bfb5d437b34f48144091eff
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 01:05:06 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,6 +27,8 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Capulet", 
+        age: "51"
         },
       :heroine => {
         #your key/value pairs here
