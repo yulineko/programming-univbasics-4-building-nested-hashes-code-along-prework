86713c38031ac212b096c11ea3629203cb5c87de
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 01:07:34 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        {},
+        {}
       ]
    }, 
    :capulet => {
